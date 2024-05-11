7fcdfeb636df975ca0b42685fe02cb7e6316c52e
Marcella Bogatzky <marcella.bogatzky@hotmail.com>
2024-05-11 15:00:42 -0300

    Aula 03

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os #Esse comando serve para importar uma biblioteca pré-existente em python
 
+restaurantes = ['Restaurante Italiano do Gepreto', 'FoodTruck do Oberyn']
+
 def exibir_nome_do_programa():
     print('''
     ╭━━━╮╱╱╭╮╱╱╱╱╱╱╱╭━━━╮
@@ -19,42 +21,54 @@ def exibir_opcoes():
     print('3. Ativar restaurante')
     print('4. Sair \n') #\n também serve para pular linha. 
 
+def exibir_subtitulo(texto):
+    os.system('cls')
+    print(texto)
+    print()
+
+def voltar_ao_menu_principal():
+    input('\n Digite uma tecla para voltar ao menu principal')
+    main()
+
+def opcao_invalida():
+    print('Opção inválida! \n')
+    voltar_ao_menu_principal()
+
+def cadastrar_novo_restaurante():
+    exibir_subtitulo('Cadastro de novos restaurantes')
+    nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
+    restaurantes.append(nome_do_restaurante) #Append coloca itens em uma lista. Corresponde ao "push" do JS
+    print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!')
+    voltar_ao_menu_principal()
+
+def listar_restaurantes():
+    exibir_subtitulo('Listando restaurantes')
+    for restaurante in restaurantes: #"Para cada" restaurante "na lista" restaurantes
+        print(f'.{restaurante}')
+    voltar_ao_menu_principal()
+
 def escolher_opcao():
-    opcao_escolhida = int(input('Escolha uma opção: ')) #Com input a gente quer trazer uma informação do usuário. "opcao_escolhida" é uma variável. O int serve para transformar a resposta em um núemero inteiro
-    print('Você escolheu a opção', opcao_escolhida) #A virgula concatena a string com a variável. Outra forma de fazer isso é: print(f'Você escolheu a opção {opcao_escolhida}')
+    try:
+        opcao_escolhida = int(input('Escolha uma opção: ')) #Com input a gente quer trazer uma informação do usuário. "opcao_escolhida" é uma variável. O int serve para transformar a resposta em um núemero inteiro
+        print('Você escolheu a opção', opcao_escolhida) #A virgula concatena a string com a variável. Outra forma de fazer isso é: print(f'Você escolheu a opção {opcao_escolhida}')
 
-    if opcao_escolhida == 1:
-        print('Cadastrar restaurante')
-    elif opcao_escolhida == 2:
-        print('Listar restaurantes')
-    elif opcao_escolhida == 3:
-        print('Ativar restaurante')
-    else:
-        finalizar_app()
-
-    # Uma alternativa ao "int" é simplesmente colocar os valores entre as aspas '', por exemplo if opcao_escolhida == '1'
-    # Outra alternativa é o uso do "match", que deixaria a construção desta forma:
-    # opcao_escolhida = int(input('Escolha uma opção: '))
-    # match opcao_escolhida:
-    #     case 1:
-    #         print('Adicionar restaurante')
-    #     case 2:
-    #         print('Listar restaurantes')
-    #     case 3:
-    #         print('Ativar restaurante')
-    #     case 4:
-    #         print('Finalizar app')
-    #     case _:  # O "_" representa qualquer outro valor diferente dos demais listados
-    #         print('Opção inválida!')
-
-
-
+        if opcao_escolhida == 1:
+            cadastrar_novo_restaurante()
+        elif opcao_escolhida == 2:
+            listar_restaurantes()
+        elif opcao_escolhida == 3:
+            print('Ativar restaurante')
+        elif opcao_escolhida == 4:
+            finalizar_app()
+        else:
+            opcao_invalida()
+    except: opcao_invalida() #O "Try" no início com o "Except" ao fim serve para que o sistema TENTE entregar o resultado, mas caso não consiga, devolva a mensagem da exceçao
 
 def finalizar_app(): #def é o comando que abre uma função (função é um bloco de códigos que realiza uma ação)
-    os.system('cls') #Comando para limpar o terminal
-    print('Finalizando o app')
+    exibir_subtitulo('Finalizando o app')
  
 def main():
+    os.system('cls')
     exibir_nome_do_programa()
     exibir_opcoes()
     escolher_opcao()
